486b58aa45998b58b3e22152bff84445fe221c2c
cognozen <tools@cognozen.ai>
2024-07-01 15:52:24 +0400

    new commit

--- app/api/get-latest-chat-id/route.ts
+++ app/api/get-latest-chat-id/route.ts
@@ -2,14 +2,21 @@ import { db } from "@/lib/db";
 import { chats } from "@/lib/db/schema";
 import { NextResponse } from "next/server";
 import { desc } from "drizzle-orm";
+import { eq } from "drizzle-orm";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic"; // static by default, unless reading the request
 export const runtime = "edge"; // specify the runtime to be edge
 
 export async function GET(request: Request) {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/sign-in");
+  }
   try {
     // Fetch the latest chat id
-    const data = await db.select().from(chats).orderBy(desc(chats.id)).limit(1);
+    const data = await db.select().from(chats).where(eq(chats.userId, userId)).orderBy(desc(chats.id)).limit(1);
     if (data.length > 0) {
       //return NextResponse.json({ chatId: data[0].id, message: 'success' });
       return NextResponse.json(data[0].id);
