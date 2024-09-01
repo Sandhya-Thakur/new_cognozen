c17613a2d407e58bc9b4c495ea4d6dd86b0c2d74
cognozen <tools@cognozen.ai>
2024-09-01 11:59:56 +0400

    commit

--- app/(main)/seeAllEmotionAttentionData/page.tsx
+++ app/(main)/seeAllEmotionAttentionData/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { Header } from "./seeAllEmotionAttentionDataHeader";
+import AttentionEmotionsDataDashboard from "@/components/AllAttentionData"
 
 const AllPdfEmotionAttentionData = () => {
   return (
     <div>
       <Header />
+      <AttentionEmotionsDataDashboard/>
+
     </div>
   );
 };
