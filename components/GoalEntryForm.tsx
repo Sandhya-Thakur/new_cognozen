135cc1e344454fbbceef79aedd1199c051b25535
cognozen <tools@cognozen.ai>
2024-10-02 02:42:29 +0400

    emotionsTracker dashboard and seeAllEmotionAttentionDatagot updated

--- components/GoalEntryForm.tsx
+++ components/GoalEntryForm.tsx
@@ -132,7 +132,7 @@ const GoalEntryForm: React.FC = () => {
   return (
     <div className="w-full max-w-4xl mx-auto p-4">
       <Toaster position="top-center" reverseOrder={false} />
-      <h2 className="text-xl font-bold mb-2 text-gray-800">Fee Good Goals</h2>
+      <h2 className="text-xl font-bold mb-2 text-gray-800">Feel Good Goals</h2>
       <p className="text-gray-600 mb-6">
         Set goals to enhance your emotional well-being.
       </p>
